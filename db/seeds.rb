54be6bc3a1824f291ee715f41f9c6cb0e5661d50
ANGEL <acostalarangeira@gmail.com>
2019-05-12 19:23:05 -0300

    fix spec and add db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Recipe.destroy_all
+
+Recipe.create(title: 'Bolo de cenoura', recipe_type: 'Sobremesa',
+  cuisine: 'Brasileira', difficulty: 'Médio',
+  cook_time: 60,
+  ingredients: 'Farinha, açucar, cenoura',
+  cook_method: 'Cozinhe a cenoura, corte em pedaços pequenos, misture com o restante dos ingredientes')
+
+Recipe.create(title: 'Feijoada', recipe_type: 'Prato Principal',
+    cuisine: 'Brasileira', difficulty: 'Difícil',
+    cook_time: 90)
