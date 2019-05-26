e69df52f89a79288dfb207a54543b216dd96a13d
ANGEL <acostalarangeira@gmail.com>
2019-05-25 23:56:20 -0300

    Add form to recipe with test

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,9 +3,30 @@ class RecipesController < ApplicationController
     @recipe = set_recipe()
   end
 
+  def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      flash[:alert] = 'Receita criada'
+      redirect_to recipe_path(@recipe)
+    else
+      flash[:alert] = 'Policia do zap, parado'
+      render :new
+    end
+  end
+  
+  def new
+    @recipe = Recipe.new
+  end
+
   private
 
   def set_recipe
     recipe = Recipe.find(params[:id])
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, :recipe_type, :cuisine, :difficulty, :cook_time, :ingredients, :cook_method)
   end
 end
